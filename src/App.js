d2c11f8d6a038d693b60a3c670a1640d90f6cd71
ikra01 <ikramajid1@gmail.com>
2022-10-08 13:05:39 +0100

    testing

--- src/App.js
+++ src/App.js
@@ -6,8 +6,11 @@ function App() {
      
       <h1>My React App</h1>
       <h2>Deploy react app test</h2>
+      <h2>changes added</h2>
     </div>
   );
 }
 
 export default App;
+// LINK
+// https://www.youtube.com/watch?v=F8s4Ng-re0E 
